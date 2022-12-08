ec6aced1936add058cd6691bb647ea7c8ea17ee3
abkubatt <abkubatt@gmail.com>
2022-12-09 00:03:07 +0600

    added RegisterVC to register new user with email and password

--- Twitter_/Controllers/Onboarding/OnboardingViewController.swift
+++ Twitter_/Controllers/Onboarding/OnboardingViewController.swift
@@ -57,7 +57,15 @@ class OnboardingViewController: UIViewController {
         view.addSubview(createAccountButton)
         view.addSubview(promptLabel)
         view.addSubview(loginButton)
+        
+        createAccountButton.addTarget(self, action: #selector(didTapCreateAccount), for: .touchUpInside)
+        
         configureConstraints()
+    }
+    
+    @objc private func didTapCreateAccount() {
+        let vc = RegisterViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     
