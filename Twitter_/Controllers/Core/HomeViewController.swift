2ad35c9cc22b82adbae9de142c5b1631ed31733f
abkubatt <abkubatt@gmail.com>
2022-12-08 22:46:13 +0600

    added Firebase packages to save data, added check user to sign in, added OnboardingVC to show not sign in user

--- Twitter_/Controllers/Core/HomeViewController.swift
+++ Twitter_/Controllers/Core/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
     
@@ -54,6 +55,12 @@ class HomeViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = false
+        
+        if Auth.auth().currentUser == nil {
+            let vc = UINavigationController(rootViewController: OnboardingViewController())
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: false)
+        }
     }
     
 
